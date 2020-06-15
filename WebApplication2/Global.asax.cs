ea04941dd65b6939804a75431e7b4c80a13dcbf7
Administrator <Administrator@PC269>
2020-06-15 09:39:22 +0700

    update something

--- WebApplication2/Global.asax.cs
+++ WebApplication2/Global.asax.cs
@@ -12,6 +12,7 @@ namespace WebApplication2
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
